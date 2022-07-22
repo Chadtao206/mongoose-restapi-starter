cac5f722689b755474047d95410db931b045ad75
Chad Tao <chenchen408@gmail.com>
2022-07-21 17:25:30 -0700

    ok

--- bin.js
+++ bin.js
@@ -1,1 +1,9 @@
-console.log("BIN!")
+#!/usr/bin/env node
+const { prompt } = require("inquirer");
+prompt({
+    message: "Welcome to mongoose restapi starter, would you like some assistance with your homework?",
+    type: "confirm",
+    name: "wantHelp"
+}).then(({wantHelp})=> {
+    console.log(wantHelp, "HELP?")
+})
