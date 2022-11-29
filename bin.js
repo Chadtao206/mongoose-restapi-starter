4c4b2d26cfd74a49bb1476f4da0692fc20ea98c7
Chad Tao <chenchen408@gmail.com>
2022-11-29 10:17:23 -0800

    added hw starter for 10-OOP

--- bin.js
+++ bin.js
@@ -1,46 +1,43 @@
 #!/usr/bin/env node
 const { prompt } = require("inquirer");
-const fs = require("fs");
+const { writeFileSync, mkdirSync } = require("fs");
 const { execSync } = require("child_process");
-const lib = require("./lib.js");
-prompt({
-  message:
-    "Welcome to mongoose restapi starter, would you like some assistance with your homework?",
-  type: "confirm",
-  name: "wantHelp",
-}).then(({ wantHelp }) => {
+const lib = require("./lib");
+
+const main = async () => {
+  const { wantHelp } = await prompt({
+    message:
+      "Welcome to bootcamp homework starter, would you like some assistance with your homework?",
+    type: "confirm",
+    name: "wantHelp",
+  });
+
   if (!wantHelp) {
     console.log("OK BYE!");
     process.exit();
   }
 
+  const { homework } = await prompt({
+    message: "What homework would you like to start?",
+    type: "list",
+    name: "homework",
+    choices: Object.keys(lib),
+  });
+
   console.log("✨✨✨ I GOT YOU FAM! ✨✨✨");
   console.log("Generating your files...");
-  fs.writeFileSync("./package.json", lib.packageJSON);
-  fs.writeFileSync("./.gitignore", lib.gitIgnore);
-  fs.writeFileSync("./server.js", lib.server);
 
-  fs.mkdirSync("config", { recursive: true });
-  fs.mkdirSync("models", { recursive: true });
-  fs.mkdirSync("routes", { recursive: true });
-  fs.mkdirSync("routes/api", { recursive: true });
-
-  fs.writeFileSync("./config/connection.js", lib.connection);
-  fs.writeFileSync("./models/index.js", lib.modelIndex);
-  fs.writeFileSync("./models/Reaction.js", lib.reactionModel);
-  fs.writeFileSync("./models/Thought.js", lib.thoughtModel);
-  fs.writeFileSync("./models/User.js", lib.userModel);
-
-  fs.writeFileSync("./routes/index.js", lib.routesIndex);
-  fs.writeFileSync("./routes/api/index.js", lib.apiRoutesIndex);
-  fs.writeFileSync("./routes/api/thought-routes.js", lib.thoughtRoutes);
-  fs.writeFileSync("./routes/api/user-routes.js", lib.userRoutes);
+  const { directories, files } = lib[homework];
+  directories.forEach((d) => mkdirSync(d, { recursive: true }));
+  files.forEach((f) => writeFileSync(f.path, f.content));
 
   console.log("All your files have been created, installing dependencies...");
   setTimeout(() => {
     execSync("npm install");
     console.log("💩 All done! enjoy! 💩");
-    console.log("type 'nodemon' to start your application!")
+    console.log("type 'nodemon' to start your application!");
     process.exit();
-  }, 2000);
-});
+  }, 1000);
+};
+
+main();
