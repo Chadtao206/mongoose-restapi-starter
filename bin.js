b53f39fb9e2fcf80511822dfbf9a09a73231d8bc
Chad Tao <chenchen408@gmail.com>
2022-11-29 10:18:59 -0800

    message added

--- bin.js
+++ bin.js
@@ -27,15 +27,14 @@ const main = async () => {
   console.log("✨✨✨ I GOT YOU FAM! ✨✨✨");
   console.log("Generating your files...");
 
-  const { directories, files } = lib[homework];
+  const { directories, files, message } = lib[homework];
   directories.forEach((d) => mkdirSync(d, { recursive: true }));
   files.forEach((f) => writeFileSync(f.path, f.content));
 
   console.log("All your files have been created, installing dependencies...");
   setTimeout(() => {
     execSync("npm install");
-    console.log("💩 All done! enjoy! 💩");
-    console.log("type 'nodemon' to start your application!");
+    console.log(message);
     process.exit();
   }, 1000);
 };
